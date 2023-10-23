b750bdad8a083438038fb7dd769d2ccd824a3829
Nathan Gregory Bornstein <nategbornstein@gmail.com>
2023-10-23 12:56:48 -0500

    Pull Changes From Dev Branch

--- server/controllers/articleController.js
+++ server/controllers/articleController.js
@@ -1,42 +1,42 @@
 const db = require("../model/articleModel");
 
 const articleController = {};
+  
+  //Github OAuth middleware 
+  articleController.authorizeUser = (req, res, next) => {
+    console.log("authorizeUser is working");
+    return next();
+  };
 
-//Github OAuth middleware
-articleController.authorizeUser = (req, res, next) => {
-  console.log("authorizeUser is working");
-  return next();
-};
-
-//Show user's articles middleware
-articleController.getUserPage = async (req, res, next) => {
-  console.log("getUserPage with any ID param is working");
-  // const { id } = req.query;
-
-  //get username record associated with the id variable passed in as a request parameter
-  //look at id variable (imagine it's the number 1) -- select record from user table where _id = 1, and return the username
-  try {
-    const queryText = `
+  //Show user's articles middleware
+  articleController.getUserPage = async (req, res, next) => {
+    console.log("getUserPage with any ID param is working");
+    // const { id } = req.query;
+    
+    //get username record associated with the id variable passed in as a request parameter
+    //look at id variable (imagine it's the number 1) -- select record from user table where _id = 1, and return the username
+    try {
+      const queryText = `
       SELECT users.username, articles.article_title, articles.article_link
       FROM users
       INNER JOIN articles ON articles.user_id = users._id
       `;
 
-    // const params = [id];
-    const result = await db.query(queryText /*, params*/);
-    // console.log('result from getuserPage', result.rows);
-    res.locals.user = result.rows;
-    return next();
-  } catch (err) {
-    return next({
-      log: `articleController.getUserPage: ERROR : ${err}`,
-      message: {
-        err: "Error occurred in articleController.getUserPage. Check server logs for more detail",
-      },
-      status: 500,
-    });
-  }
-};
+      // const params = [id];
+      const result = await db.query(queryText /*, params*/); 
+      // console.log('result from getuserPage', result.rows);
+      res.locals.user = result.rows;
+      return next();
+    } catch (err) {
+      return next({
+        log: `articleController.getUserPage: ERROR : ${err}`,
+        message: {
+          err: "Error occurred in articleController.getUserPage. Check server logs for more detail",
+        },
+        status: 500,
+      });
+    }
+  };
 
 // Update and publish Feed Articles
 articleController.feedArticles = async (req, res, next) => {
@@ -63,36 +63,39 @@ articleController.feedArticles = async (req, res, next) => {
   }
 };
 
-//Add article middleware
-articleController.addArticle = async (req, res, next) => {
-  try {
-    console.log("articleController.addArticle middleware is working");
+  //Add article middleware
+  articleController.addArticle = async (req, res, next) => {
+    try {
+        console.log("articleController.addArticle middleware is working");
 
-    //capture article_link value and user value from front end request body
-    const { article_link, user } = req.body;
+        //capture article_link value and user value from front end request body
+        const { article_link, user } = req.body;
+        
 
-    //define query parameters ($1 = article_link, $2 = user)
-    const queryParams = [article_link, user];
+        //define query parameters ($1 = article_link, $2 = user)
+        const queryParams = [article_link, user];
 
-    //Define SQL query
-    const text = `
+
+        //Define SQL query
+        const text = `
         INSERT INTO articles (article_link, article_title, user_id)
         VALUES ($1, $2, 1)
         `;
 
-    //Run SQL query
-    const result = await db.query(text, queryParams);
-    res.locals.article = result;
-    return next();
-  } catch (err) {
-    return next({
-      log: `articleController.addArticle: ERROR : ${err}`,
-      message: {
-        err: "Error occurred in articleController.addArticle. Check server logs for more detail",
-      },
-      status: 500,
-    });
-  }
+
+        //Run SQL query
+        const result = await db.query(text, queryParams);
+        res.locals.article = result;
+        return next();
+        } catch (err) {
+        return next({
+            log: `articleController.addArticle: ERROR : ${err}`,
+            message: {
+              err: "Error occurred in articleController.addArticle. Check server logs for more detail",
+            },
+            status: 500,
+          });
+    }
 };
 
 articleController.deleteArticle = async (req, res, next) => {
@@ -113,9 +116,11 @@ articleController.deleteArticle = async (req, res, next) => {
       status: 500,
     });
   }
-};
+  };
 
 module.exports = articleController;
+
+
 
 // temporary search query test
 // SELECT u.username AS submitter, a.article_title, a.article_link
